344adad4474628e96821c6e2375a3cc35c296038
Suhas Bhairav <suhasbhairav@gmail.com>
2020-12-14 07:39:16 +0100

    Added inital practise files for numpy

---
+++ practise1.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+arr1 = np.array([10, 20, 30, 40, 50])
+print(arr1)
+
+arr2 = np.array([15, 20, 25, 30, 35])
+
+print(arr2)
+
+print(arr)
+
+
+tup = np.array((1,2,3,4,5))
+
+tup1 = np.array((10,20,30,40,50))
+print(tup1)
+print(tup)
+
+#0-D arrays
+zero_arrays = np.array(42)
+print((zero_arrays))
+
+#1-D arrays
+
+one_arrays = np.array([1,2,3,4,5])
+print(one_arrays)
+
+#1-D arrays
+one_d_arrays = np.array([10,20,30,40,50])
+print(one_d_arrays)
+
+#2-D arrays
+two_d_arrays = np.array([[1,2,3], [4,5,6]])
+print(two_d_arrays)
+
+#2-d arrays
+two_d_arr2 = np.array([[1,2,3,4,5,6], [7,8,9,10,11,12]])
+print(two_d_arr2)
+
+#3-D arrays
+three_d_arrays = np.array([[[1,2,3],[3,4,5]], [[1,2,4], [5,6,7]]])
+print(three_d_arrays)
+
+
+
+
+
+
+
